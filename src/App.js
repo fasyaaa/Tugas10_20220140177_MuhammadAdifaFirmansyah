3228db9ed88f6105c595a40c57884c633adbc953
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-06-08 23:22:42 +0700

    ganti design

--- src/App.js
+++ src/App.js
@@ -5,19 +5,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Apa Itu Node Js ?</h1>
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo1" alt="logo" />
+        <img src={logo} className="App-logo2" alt="logo" />
+      </header>
+      <section className="App-content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine
+          that allows developers to run JavaScript on the server-side.
+        </p>
+        <p>
+          It provides an event-driven, non-blocking I/O model, which makes it
+          lightweight and efficient for building scalable server-side
+          applications.
+        </p>
+      </section>
+      <footer className="App-footer">
+        <p>
+          Punya <code>Muhammad Adifa Firmansyah</code>.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/adifafirmansyah/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Follow me on Instagram!
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
